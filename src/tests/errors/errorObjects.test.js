5b223606f8fd6c6b2e95f9627b5026e021bd602f
joshxg <griffinjoshua5@gmail.com>
2019-06-06 14:22:25 +1200

    Add errorObject tests

---
+++ src/tests/errors/errorObjects.test.js
@@ -0,0 +1,17 @@
+/**
+ * Tests to ensure error object return objects are correct
+ * and consistent.
+ *
+ * Each test should use the `errorObject` method in errors/errors.js
+ * and each error code function should have it's own test.
+ */
+
+import { CORRECT_ERROR_OBJECTS, ERROR_TEST_DATA } from '../testData/errors';
+import { errorObject } from '../../errors/errors';
+
+test('Runtime error object should be equal', () => {
+  Object.entries(ERROR_TEST_DATA).forEach(([errorCode, testData]) => {
+    const returnValue = errorObject(...testData);
+    expect(returnValue).toEqual(CORRECT_ERROR_OBJECTS[errorCode]);
+  });
+});
