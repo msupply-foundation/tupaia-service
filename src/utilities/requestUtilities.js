a66e902b7e29e9eed490da917bfd1a43a71fb3f9
joshxg <griffinjoshua5@gmail.com>
2019-06-17 08:53:05 +1200

    Add fix for array mutation

--- src/utilities/requestUtilities.js
+++ src/utilities/requestUtilities.js
@@ -3,6 +3,16 @@
  */
 
 /**
+ * Simple method sorting objects with a row field in
+ * descending order.
+ */
+const sortErrorObjects = ({ row: rowA }, { row: rowB }) => {
+  if (rowA > rowB) return 1;
+  if (rowA < rowB) return -1;
+  return 0;
+};
+
+/**
  * In response to a POST request to the Tupaia API, any invalid
  * object within the post request invalidates the entire body.
  * The response to this post is an object which lists the objects
@@ -26,18 +36,16 @@ const removeInvalidObjects = (originalData, invalidData) => {
   // through the array of errors, which indicate each invalid
   // object in the original body.
   const originalDataClone = [...originalData];
+  const errorsClone = [...errors];
+  errorsClone.sort(sortErrorObjects);
   const invalidObjects = [];
-  let indexOffset = 0;
   errors.forEach(errorObject => {
     // Error object: { row, error } - where row is the index and error
     // is an error message.
     const { row } = errorObject;
     // If the row value is valid, remove the index from the original data
-    if (!Number.isInteger(Number(row)) || row >= invalidData.length || row - indexOffset < 0) {
-      return;
-    }
-    const [invalidDatum] = originalDataClone.splice(row - indexOffset, 1);
-    indexOffset += 1;
+    if (!Number.isInteger(Number(row)) || row >= invalidData.length) return;
+    const [invalidDatum] = originalDataClone.splice(row, 1);
     // If the invalidDatum exists, push it to the invalid objects array.
     if (!invalidDatum) return;
     invalidObjects.push(invalidDatum);
