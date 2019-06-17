10cc7fe08b068a66d07b689fa55c4be70a208719
joshxg <griffinjoshua5@gmail.com>
2019-06-17 12:08:48 +1200

    Add simple surveyResponse test

--- src/tests/utilities.test.js
+++ src/tests/utilities.test.js
@@ -3,10 +3,10 @@ import { removeInvalidObjectsTestData } from './testData/utilities';
 
 /**
  * Test cases:
- * 1 - removing all data, as all have errors
- * 2 - removing one object, as only one has an error
- * 3 - removing no data, as none have errors
- * 4 - returning empty arrays as no data has been passed
+ * 1 : removing all data, as all have errors
+ * 2 : removing one object, as only one has an error
+ * 3 : removing no data, as none have errors
+ * 4 : returning empty arrays as no data has been passed
  */
 test('Should pass all tests', () => {
   Object.values(removeInvalidObjectsTestData).forEach(({ INPUT, OUTPUT }) => {
