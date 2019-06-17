98cf0e8d4ff5109aa9a0fcab07731228a4a55149
joshxg <griffinjoshua5@gmail.com>
2019-06-18 11:36:48 +1200

    Add update to test case comments

--- src/tests/utilities.test.js
+++ src/tests/utilities.test.js
@@ -7,6 +7,8 @@ import { removeInvalidObjectsTestData } from './testData/utilities';
  * 2 : removing one object, as only one has an error
  * 3 : removing no data, as none have errors
  * 4 : returning empty arrays as no data has been passed
+ * 5 : calling with an empty originalData parameter
+ * 6 : calling with out of place parameters
  */
 test('Should pass all tests', () => {
   Object.values(removeInvalidObjectsTestData).forEach(({ INPUT, OUTPUT }) => {
