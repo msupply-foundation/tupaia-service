b126c267a3d8623c79b9bf3e34ae66dec90ef63a
joshxg <griffinjoshua5@gmail.com>
2019-06-21 10:18:02 +1200

    Add update to utlitity method tests

--- src/tests/testData/utilities.js
+++ src/tests/testData/utilities.js
@@ -3,37 +3,43 @@
 export const removeInvalidObjectsTestData = {
   TEST_ONE: {
     INPUT: {
-      originalData: [1, 2, 3],
+      originalData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }],
       invalidData: { errors: [{ row: 0 }, { row: 1 }, { row: 2 }] },
     },
-    OUTPUT: { data: [], invalidData: [3, 2, 1] },
+    OUTPUT: { validData: [], invalidData: [{ 3: 3, row: 2 }, { 2: 2, row: 1 }, { 1: 1, row: 0 }] },
   },
   TEST_TWO: {
-    INPUT: { originalData: [1, 2, 3], invalidData: { errors: [{ row: 2 }] } },
-    OUTPUT: { data: [1, 2], invalidData: [3] },
+    INPUT: { originalData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }], invalidData: { errors: [{ row: 2 }] } },
+    OUTPUT: { validData: [{ 1: 1 }, { 2: 2 }], invalidData: [{ 3: 3, row: 2 }] },
   },
   TEST_THREE: {
-    INPUT: { originalData: [1, 2, 3], invalidData: {} },
-    OUTPUT: { data: [1, 2, 3], invalidData: [] },
+    INPUT: { originalData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }], invalidData: {} },
+    OUTPUT: { validData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }], invalidData: [] },
   },
   TEST_FOUR: {
     INPUT: {},
-    OUTPUT: { data: [], invalidData: [] },
+    OUTPUT: { validData: [], invalidData: [] },
   },
   TEST_FIVE: {
     INPUT: { originalData: [], invalidData: { errors: [{ row: 0 }, { row: 2 }] } },
-    OUTPUT: { data: [], invalidData: [] },
+    OUTPUT: { validData: [], invalidData: [] },
   },
   TEST_SIX: {
-    INPUT: { originalData: [1, 2, 3], invalidData: { errors: [{ row: 0 }, { row: 2 }] } },
-    OUTPUT: { data: [2], invalidData: [3, 1] },
+    INPUT: {
+      originalData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }],
+      invalidData: { errors: [{ row: 0 }, { row: 2 }] },
+    },
+    OUTPUT: {
+      validData: [{ 2: 2 }],
+      invalidData: [{ 3: 3, row: 2 }, { 1: 1, row: 0 }],
+    },
   },
   TEST_SEVEN: {
-    INPUT: { originalData: [1, 2, 3], invalidData: { errors: [] } },
-    OUTPUT: { data: [1, 2, 3], invalidData: [] },
+    INPUT: { originalData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }], invalidData: { errors: [] } },
+    OUTPUT: { validData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }], invalidData: [] },
   },
   TEST_EIGHT: {
-    INPUT: { originalData: [1, 2, 3], invalidData: { errors: [{ row: 4 }] } },
-    OUTPUT: { data: [1, 2, 3], invalidData: [] },
+    INPUT: { originalData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }], invalidData: { errors: [{ row: 4 }] } },
+    OUTPUT: { validData: [{ 1: 1 }, { 2: 2 }, { 3: 3 }], invalidData: [] },
   },
 };
